23c5e7f41cbb4716436d7d1401f187ac73aa474c
Grinmak <grinmak@gmail.com>
2022-08-16 19:15:21 +0300

    add function to compare both password inputs and function to prevent submit until paswords match

--- script.js
+++ script.js
@@ -1,32 +1,73 @@
 const pass = document.getElementById('pass');
 const passCheck = document. getElementById('confirmPass');
-console.log(passCheck);
 
 let password ='';
 let confirmPassword = '';
 
 pass.addEventListener('input', customErrorMesagge);
+pass.addEventListener('keydown', passEdit);
+passCheck.addEventListener('input', passConfirm);
+passCheck.addEventListener('keydown', passEdit);
+
+form.addEventListener('submit', preventSubmit);
+
 function customErrorMesagge (event){
+
     classTogle();
-    password += event.data;
-    console.log(event);
+    if(event.data !== null){
+        password += event.data;
+    };
     if(pass.validity.patternMismatch) {
         pass.setCustomValidity('Password should contain at least one upper and lower case letters and one digit');
     } else pass.setCustomValidity('');
+    console.log(password);
 };
-/*     if(pass.validity.tooShort) {
-        pass.setCustomValidity('Minimal length is 8 characters')
-    } else pass.setCustomValidity(''); */
-
-
-passCheck.addEventListener('input', passConfirm);
-function passConfirm (event) {
-    console.log(event.data);
-}
 
 function classTogle () {
     if(pass.validity.valid) {
-       /*  pass.classList.toggle('success'); */
        document.getElementById('pass').className = 'success';
-     }
+     };
+};
+
+function passConfirm (event) {
+
+    if(event.data !== null){
+        confirmPassword += event.data;
+    };
+    if( password !== confirmPassword){ 
+        if(passCheck.validity.patternMismatch){
+            passCheck.setCustomValidity('Passwords doesn\'t match');
+        };
+
+    };
+
+    if(password === confirmPassword){
+        document.getElementById('confirmPass').className = 'success';
+    }; 
+    console.log(confirmPassword);
+};
+
+
+
+
+function passEdit (event) {
+
+    if(event.key === 'Backspace'){
+        if(event.target.id === 'confirmPass'){
+            confirmPassword = confirmPassword.substring(0, confirmPassword.length-1);
+        };
+        if(event.target.id === 'pass'){
+            password = password.substring(0, password.length-1);
+        }
+    }
+    console.log(confirmPassword);
+}; 
+
+function preventSubmit (event) {
+    if(passCheck.className !== 'success') {
+        passCheck.setCustomValidity('Passwords doesn\'t match');
+        event.preventDefault();
+
+
+    };
 }
